24ed1cc9b3239c2d0cc4ca68f483956b36ae481f
drg809 <drg809@gmail.com>
2023-06-06 19:23:58 +0200

    removeImage and editImage methods

--- src/redux/states/image.ts
+++ src/redux/states/image.ts
@@ -1,6 +1,6 @@
 import { LocalStorageTypes, Image } from '@/models';
 import { getLocalStorage, setLocalStorage } from '@/utilities';
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, current } from '@reduxjs/toolkit';
 
 const initialState: Image[] = [];
 
@@ -9,13 +9,25 @@ export const imagesSlice = createSlice({
   initialState: getLocalStorage(LocalStorageTypes.IMAGES) ? JSON.parse(getLocalStorage(LocalStorageTypes.IMAGES) as string) : initialState,
   reducers: {
     addImage: (state, action) => {
-      const current = [...getLocalStorage(LocalStorageTypes.IMAGES) ? JSON.parse(getLocalStorage(LocalStorageTypes.IMAGES) as string) : initialState, action.payload]
-      setLocalStorage(LocalStorageTypes.IMAGES, current);
-      return action.payload;
+      const images = [...getLocalStorage(LocalStorageTypes.IMAGES) ? JSON.parse(getLocalStorage(LocalStorageTypes.IMAGES) as string) : initialState, action.payload]
+      setLocalStorage(LocalStorageTypes.IMAGES, images);
+      return images;
+    },
+    removeImage: (state, action) => {
+      const filteredState = current(state).filter((p: Image) => p.id !== action.payload.id);
+      setLocalStorage(LocalStorageTypes.IMAGES, filteredState);
+      return filteredState;
+    },
+    editImage: (state, action) => {
+      const filteredState = current(state).filter((p: Image) => p.id !== action.payload.id);
+      const stateToEdit = current(state).find((p: Image) => p.id === action.payload.id);
+      const images = [...filteredState, <Image>{id: stateToEdit.id, title: action.payload.title, image: stateToEdit.image}];
+      setLocalStorage(LocalStorageTypes.IMAGES, filteredState);
+      return images;
     }
   }
 });
 
-export const { addImage } = imagesSlice.actions;
+export const { addImage, removeImage, editImage } = imagesSlice.actions;
 
 export default imagesSlice.reducer;
